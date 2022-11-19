3abebe3fa30fdf9514f6b9d479b467782477e579
LukVill <luke.lam.villanueva@gmail.com>
2022-11-18 17:34:12 -0800

    finished printing out probabilities, however we don't know if the dimensions are 16x16 since the table IMPLIES that there is an Age Class 17

--- proj 3.1 bobcat HW5.py
+++ proj 3.1 bobcat HW5.py
@@ -1,3 +1,4 @@
+from xml.dom.pulldom import DOMEventStream
 import numpy as np
 from icecream import ic
 
@@ -5,7 +6,7 @@ from icecream import ic
 """
 MAKE 16 X 16 MATRIX
 """
-
+DIMENSION = 16
 #make initial row
 A =np.array([0.63,0.63])
 #append 14 1.2's
@@ -18,7 +19,7 @@ A = np.mat(A)
 
 r = np.array([0.34])
 #add 15 0's
-for x in range(15):
+for x in range(DIMENSION-1):
     r = np.append(r,[0])
 ic(r)
 
@@ -28,7 +29,7 @@ A = np.r_[A,[r]]
 
 #every following row will be similar form, so use for loop
 #append 14 rows
-for x in range(14):
+for x in range(DIMENSION-2):
     temp = [0.0] * 16
     #change into np array
     temp = np.array(temp)
@@ -40,8 +41,8 @@ ic(A)
 #get eigenvalues
 eVal,eVec = np.linalg.eig(A)
 
-ic(eVal)
-ic(eVec)
+print("Eigenvalues: \n",eVal)
+print("Eigenvectors: \n",eVec)
 
 #find real evals and their respective evectors
 realVal = np.array([]) #array of real evals
@@ -56,9 +57,6 @@ realVal = eVal[idx]
 for x in idx:
     realVec = np.c_[eVec[:,x]]
 
-testmat = np.mat([[1,2],[0,3]])
-testval,testvec = np.linalg.eig(testmat)
-
 print("\n")
 
 print("Eigenvalues of the Bobcat Transition Matrix:")
@@ -66,13 +64,45 @@ print(realVal,"\n")
 print("Example Eigenvectors of the Bobcat Transition Matrix (columns respective to eigenvalue indices):")
 print(realVec,"\n where column @index 0 is the e-vector for e-val @ index 0, and column @ index 1 is the e-vector for e-val @ index 1")
 
-"""
-TODO:
-i have acquired eigenvalues and eigenvectors
-reference the notes, but the evals are row oriented. their index corresponds to the evec's but on the column
-meaning, eval @ 1 is eval for the evec @ [:,1]
+#sum column 1
+sum = 0
+for x in range(DIMENSION):
+    sum+=realVec[x,0]
+sum = np.real(sum)
+print("Sum of Population Vectors of the Dominant Eigenvalue's Eigenvector: \n",sum)
 
-need to apply matrix to prob. vector for 10 iterations
+
+#make dom eval array and dom eval eigen vector
+domEval = np.max(realVal)
+domEval.reshape(1,1)
+idx = np.where(np.max(realVal))
+ic(idx)
+domEvec = realVec[:,idx]
+domEvec = domEvec.flatten()
+domEvec = np.real(domEvec)
+
+ic(domEvec)
+ic(domEvec.shape)
+
+domEvec = np.multiply(domEvec,np.reciprocal(sum))
+
+print("This is the eigenvector with its values summing to 1 (i.e. proportion vector):",domEvec)
+
+total = 0
+for x in range(DIMENSION):
+    total += domEvec[0,x]
+
+print("Total of eigenvector values: \n",total,"\n So, we know the values are proportions now")
+for x in range(DIMENSION):
+    print("Bobcat Age Class - ", x+1, " Proportion: ",np.format_float_positional(domEvec[0,x]))
+
+
+"""
+TODO: 
+wait for professor to clarify if the table implies that there is an 
+Age Class 17
+
+Since there is a survival rate for Age Class 16 is 0.71
 """
 
 """
