2440e194924347ae1c23427e5af9903223b9b50e
LukVill <luke.lam.villanueva@gmail.com>
2022-11-22 01:20:06 -0800

    need to work on displaying more than 1 dataframe in the same graph

--- proj 3.1 bobcat HW5.py
+++ proj 3.1 bobcat HW5.py
@@ -1,5 +1,17 @@
 import numpy as np
 from icecream import ic
+import random as rd
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+
+DIMENSION = 17
+
+INITIAL_POP = np.r_[0,100,50,50,25,10, [0]*11]
+
+print("Best Case Rates for Bobcat Population: \n")
 
 
 """
@@ -10,45 +22,40 @@ MAKE 17X17 MATRIX
 iterate until last row
 last row: 0 ... 0.71 0
 """
-DIMENSION = 17
+def bestCaseMatrix():
+    #make initial row
+    m =np.array([0.63,0.63])
+    #append 14 1.2's (bc class 17 doesnt have a birth rate)
+    for x in range(DIMENSION-3):
+        m = np.append(m,[1.2])
+    #append a 0 at the end for class 17
+    m = np.append(m,0)
 
-print("Best Case Rates for Bobcat Population: \n")
+    m = np.mat(m)
 
-#make initial row
-matA =np.array([0.63,0.63])
-#append 14 1.2's (bc class 17 doesnt have a birth rate)
-for x in range(DIMENSION-3):
-    matA = np.append(matA,[1.2])
-#append a 0 at the end for class 17
-matA = np.append(matA,0)
+    #make second row
 
-matA = np.mat(matA)
+    r = np.array([0.34])
+    #add 16 0's
+    for x in range(DIMENSION-1):
+        r = np.append(r,[0])
+    #append second row to first
 
-ic(matA.shape)
+    m = np.r_[m,[r]]
 
-#make second row
+    #every following row will be similar form, so use for loop
+    #append 15 rows
+    for x in range(DIMENSION-2):
+        temp = [0.0] * (DIMENSION)
+        #change into np array
+        temp = np.array(temp)
+        temp[x+1] = 0.71
+        #append row to matrix
+        m = np.r_[m,[temp]]
+    return m
 
-r = np.array([0.34])
-#add 16 0's
-for x in range(DIMENSION-1):
-    r = np.append(r,[0])
-ic(r)
-
-#append second row to first
-
-matA = np.r_[matA,[r]]
-
-#every following row will be similar form, so use for loop
-#append 15 rows
-for x in range(DIMENSION-2):
-    temp = [0.0] * (DIMENSION)
-    #change into np array
-    temp = np.array(temp)
-    temp[x+1] = 0.71
-    #append row to matrix
-    matA = np.r_[matA,[temp]]
-ic(matA)
-ic(matA.shape)
+matA = bestCaseMatrix()
+print(matA)
 
 # Parameters: A = Numpy matrix 
 def displayEigen(A):
@@ -83,11 +90,13 @@ def displayEigen(A):
     domEvalidx = realVal.argmax()
     domEval = realVal.max()
 
+    print("\n\nDominant Eigenvalue:",domEval)
+
     sum = 0
     for x in range(DIMENSION):
         sum+=realVec[x,domEvalidx]
     sum = np.real(sum)
-    print("Sum of Population Vectors of the Dominant Eigenvalue's Eigenvector: \n",sum)
+    print("\n\nSum of Population Vectors of the Dominant Eigenvalue's Eigenvector: \n",sum)
 
 
     #make dom eval array and dom eval eigen vector
@@ -115,8 +124,7 @@ APPLYING MATRIX OVER 10 YEARS
 #transition matrix is variable A
 
 #make initial probability vector 
-init = np.r_[0,100,50,50,25,10, [0]*11]
-ic(len(init))
+init = INITIAL_POP
 print("\n Initial Population Vector: \n", init)
 
 # Parameters: mat = matrix to multiply initials with, init = initial probability vectors,
@@ -172,17 +180,114 @@ Matrix Composition
 print("\n\n-------------------------------\n\n")
 print("Worst Case Rates for Bobcat Population: \n")
 
-# make first row
-matA = np.array([0.6,0.6])
-# append 14 1.15's
-for x in range(14):
-    matA = np.append(matA,1.15)
-#append a 0
-matA = np.append(matA,0)
-matA = np.mat(matA)
-# make second row
-r = np.array([0.32])
-# append 16 0's
-for x in range(16):
-    r = np.append(r,0)
-#make rows 3 -
+def worstCaseMatrix():
+    # make first row
+    m = np.array([0.6,0.6])
+    # append 14 1.15's
+    for x in range(DIMENSION-3):
+        m = np.append(m,1.15)
+    #append a 0
+    m = np.append(m,0)
+    m = np.mat(m)
+
+    # make second row
+    r = np.array([0.32])
+    # append 16 0's
+    for x in range(DIMENSION-1):
+        r = np.append(r,0)
+    #append second row
+    m = np.r_[m,[r]]
+ 
+    #make rows 3-17
+    for x in range(DIMENSION-2):
+        r = np.array([0.0]*DIMENSION)
+        r[x+1] = 0.68
+        m = np.r_[m,[r]]
+    return m
+
+matA = worstCaseMatrix()
+
+print(matA)
+
+#calculate worstMatrix eigen vals and vecs
+displayEigen(matA)
+
+#initial conditions
+#make initial probability vector 
+init = INITIAL_POP
+print("\n Initial Population Vector: \n", init)
+
+simulate(matA,init,10)
+
+
+"""
+
+REPEAT FOR BEST CASE MATRIX BUT HAVE CATASTROPHE CHANCE
+
+"""
+
+#let matA = best case
+matA = bestCaseMatrix()
+
+#create function that returns bool if catastrophe occurs
+def isCatastrophe():
+    #generate num from 1-25
+    val = rd.randint(1,25)
+    if val == 1:
+        return True
+    else:
+        return False
+    
+# create function that calculates iteration of the matrix FOR ONE YEAR
+# if the 
+#Returns matrix and resulting population vector
+def simNoPrint(matrix,initial):
+    #check if catastrophe occurs
+    catastrophe = isCatastrophe()
+    if catastrophe == True:
+        matrix[0,:] *= 0.7
+    initial = np.matmul(matrix,initial)
+    initial = np.reshape(initial,(DIMENSION,1))
+    return matrix,initial
+
+#initial conditions
+#make initial probability vector 
+init = INITIAL_POP
+print("\n Initial Population Vector: \n", init)
+
+df = pd.DataFrame()
+
+for x in range(2):
+    #create data fram for one simulation
+    for x in range(10):
+        matB = matA
+        initB = init
+        matB, initB = simNoPrint(matB,initB)
+        res = initB.tolist()
+        df["Year",x+1] = res
+
+
+    """
+    EXTRACT DATA FROM DATAFRAME
+    FORMAT AS DICTIONARY 
+    """
+    data = []
+    #for each indiviual columns in total columns
+    for targetCol in df.columns:
+        #for each index and cell returns by items() operator
+        for indx, cell in df[targetCol].items():
+            #for each value IN the cell
+            for val in cell:
+                #set dictionary with labels
+                #age group is index+1, 
+                d = {"Age Group":indx+1,"Years":targetCol[1],"Population Count":val}
+                data.append(d)
+    print(data)
+    finalData = pd.DataFrame(data)
+
+    print("\n\n",finalData)
+
+    graph = sns.lineplot(data = finalData, x = "Years", y = "Population Count", hue = "Age Group", legend = "full")
+
+graph.legend(fontsize = 5, title = "Age Group:", shadow = True)
+plt.show()
--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-print("Hello World!")
-print("Version 2")
-print("Child 1")
+import random as rd
+
+success = 0
+fail = 0
+for x in range(1000):
+    val = rd.randint(1,25)
+    print(val)
+    if val == 1:
+        success+=1
+    else:
+        fail += 1
+print(success/fail)
