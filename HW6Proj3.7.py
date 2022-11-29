6f165674362ffab6585c8629ed17b7b44318c5f0
LukVill <luke.lam.villanueva@gmail.com>
2022-11-29 14:34:22 -0800

    now just trying to graph

--- HW6Proj3.7.py
+++ HW6Proj3.7.py
@@ -8,18 +8,18 @@ import seaborn as sns
 # form nxn with 5 degrees on top, left, right, and 0 on bottom
 def nxn(row,col):
     #set first row
-    matrix = np.array([5]*col)
+    matrix = np.array([5.0]*col)
     matrix = np.mat(matrix)
     ic(matrix)
 
     #set edges to 5
     for x in range(row-2):
-        r = np.array(5)
+        r = np.array(5.0)
         r = np.append(r,[None]*(col-2))
-        r = np.append(r,5)
+        r = np.append(r,5.0)
         matrix = np.r_[matrix,[r]]
     # set bottom row to 0
-    matrix = np.r_[matrix, [[0]*col]]
+    matrix = np.r_[matrix, [[0.0]*col]]
     return matrix
 
 
@@ -127,88 +127,88 @@ def tempModel(initialCond):
 
     # Make diagonal of A be all 4's
     for x in range(len(A)):
-        A[x,x] = 4
+        A[x,x] = 4.0
 
+    # Array indices
+    i = 0
+    j = 0
     # Check properties of cells
-    for i in range(nrows):
-        for j in range(ncols):
-            # if true, then current index is Temp node
-            if initialCond[i,j] == None:
-                node_count+=1
-
-                # Check up, left, right, and down of curr cell
-                # Case 1: Check up
-                # boundary check
-                if i > 0:
-                    # if curr index is Temp node
-                    if initialCond[i-1,j] == None:
-                        # Get distance
-                        d = distanceParser(initialCond, i, j, i-1, j, \
-                            isNegative = True)
-                        order = node_count-d
-                        # Mark A a -1 at order of Temp node
-                        A[node_count-1][order-1] = -1
-                    # if not Temp node
-                    else:
-                        # add value to B 
-                        B[node_count-1] += initialCond[i-1,j]
-                # Case 2: Check down
-                # boundary check
-                if i < nrows-1:
-                    # if curr index is Temp node
-                    if initialCond[i+1,j] == None:
-                        # Get distance
-                        d = distanceParser(initialCond, i, j, i+1, j, \
-                            isNegative = False)
-                        order = node_count + d
-                        # Mark A a -1 at order of Temp node
-                        A[node_count-1][order-1] = -1
-                    # if not Temp node
-                    else:
-                        # add value to B 
-                        B[node_count-1] += initialCond[i+1,j]
-                # Case 3: Check left
-                # boundary check
-                if j > 0:
-                    # if curr index is Temp node
-                    if initialCond[i,j-1] == None:
-                        # Get distance
-                        d = distanceParser(initialCond, i, j, i, j-1, \
-                            isNegative = True)
-                        order = node_count - d
-                        # Mark A a -1 at order of Temp node
-                        A[node_count-1][order - 1] = -1
-                    # if not Temp node
-                    else:
-                        # add value to B 
-                        B[node_count-1] += initialCond[i,j-1]
-                # Case 3: Check right
-                # boundary check
-                if j < ncols-1:
-                    # if curr index is Temp node
-                    if initialCond[i,j+1] == None:
-                        # Get distance
-                        d = distanceParser(initialCond, i, j, i, j+1, \
-                            isNegative = False)
-                        order = node_count + d
-                        # Mark A a -1 at order of Temp node
-                        A[node_count-1][order - 1] = -1
-                    # if not Temp node
-                    else:
-                        # add value to B 
-                        B[node_count-1] += initialCond[i,j+1]
-
-            # whether true or false, parse
-            # check if reached the end of initialCond matrix
-            if i == nrows-1 and j == ncols -1:
-                return A,B 
-            # if at last col, move to next row
-            if j == ncols - 1:
-                i += 1
-                j = 0
-            # else, normal column parse
-            else:
-                j+=1
+    for iterate in range(nrows*ncols):
+        # if true, then current index is Temp node
+        if initialCond[i,j] == None:
+            node_count+=1               
+            # Check up, left, right, and down of curr cell
+            # Case 1: Check up
+            # boundary check
+            if i > 0:
+                # if curr index is Temp node
+                if initialCond[i-1,j] == None:
+                    # Get distance
+                    d = distanceParser(initialCond, i, j, i-1, j, \
+                        isNegative = True)
+                    order = node_count-d
+                    # Mark A a -1 at order of Temp node
+                    A[node_count-1][order-1] = -1.0
+                # if not Temp node
+                else:
+                    # add value to B 
+                    B[node_count-1] += initialCond[i-1,j]
+            # Case 2: Check down
+            # boundary check
+            if i < nrows-1:
+                # if curr index is Temp node
+                if initialCond[i+1,j] == None:
+                    # Get distance
+                    d = distanceParser(initialCond, i, j, i+1, j, \
+                        isNegative = False)
+                    order = node_count + d
+                    # Mark A a -1 at order of Temp node
+                    A[node_count-1][order-1] = -1.0
+                # if not Temp node
+                else:
+                    # add value to B 
+                    B[node_count-1] += initialCond[i+1,j]
+            # Case 3: Check left
+            # boundary check
+            if j > 0:
+                # if curr index is Temp node
+                if initialCond[i,j-1] == None:
+                    # Get distance
+                    d = distanceParser(initialCond, i, j, i, j-1, \
+                        isNegative = True)
+                    order = node_count - d
+                    # Mark A a -1 at order of Temp node
+                    A[node_count-1][order - 1] = -1.0
+                # if not Temp node
+                else:
+                    # add value to B 
+                    B[node_count-1] += initialCond[i,j-1]
+            # Case 3: Check right
+            # boundary check
+            if j < ncols-1:
+                # if curr index is Temp node
+                if initialCond[i,j+1] == None:
+                    # Get distance
+                    d = distanceParser(initialCond, i, j, i, j+1, \
+                        isNegative = False)
+                    order = node_count + d
+                    # Mark A a -1 at order of Temp node
+                    A[node_count-1][order - 1] = -1.0
+                # if not Temp node
+                else:
+                    # add value to B 
+                    B[node_count-1] += initialCond[i,j+1]
+        # whether true or false, parse
+        # check if reached the end of initialCond matrix
+        if i == nrows-1 and j == ncols -1:
+            return A,B 
+        # if at last col, move to next row
+        if j == ncols - 1:
+            i += 1
+            j = 0
+        # else, normal column parse
+        else:
+            j+=1
 
 
     # Assert to double check parsing logic
@@ -220,17 +220,15 @@ def tempModel(initialCond):
 # function to find values of T1-Tn
 # Parameters: transition matrix, initial conditions of temperature 
 # based on diagram
-# Returns: internal matrix of T's (EXCLUDES INITIAL CONDITIONS)
+# Returns: internal vector of T's
 def findTNodes(transMat, initialCond):
     A = transMat
     B = initialCond
-    ic(A.shape)
-    ic(B.shape)
     Ainv = np.linalg.inv(A)
-    ic(Ainv)
+    print("\nInverse of A: ", A,"\n")
     # To solve for T, T = A^-1B
     T = np.matmul(Ainv,B)
-    ic(T)
+    return T
 
 # insertTNodes
 # Function: inserts TNodes into initial condition matrix (aka,
@@ -250,19 +248,48 @@ def insertTNodes(Tvec,initialCond):
     # Node counter to track which node we are on
     node_counter = 0
 
+
+    # Array indices
+    i = 0
+    j = 0
     # For each element in initialCond
-    for i in range(nrows):
-        for j in range(ncols):
-            # if the elem is node
-            if initialCond[i,j] == None:
-                # increment counter
-                node_counter+=1
-                # extract value in resultant vector
-                initialCond[i,j] = Tvec[node_counter-1] #-1 bc index
-            # whether found or not, parse
-                 
+    for iterate in range(nrows*ncols):
+        # if the elem is node
+        if initialCond[i,j] == None:
+            # increment counter
+            node_counter+=1
+            # extract value in resultant vector
+            initialCond[i,j] = Tvec[node_counter-1] #-1 bc index
+        # whether found or not, parse
+        # if at last col, move to next row
+        if j == ncols - 1:
+            i += 1
+            j = 0
+        # else, normal column parse
+        else:
+            j+=1
+
+    # When finished, return newly edited initialCond
+    return initialCond
+
+
+
 mat = nxn(5,5)
 A,B = tempModel(mat)
 ic(A)
 T = findTNodes(A,B)
 ic(T)
+result = insertTNodes(T,mat)
+ic(result)
+
+# convert all types in
+
+# turn result into datafram
+df = pd.DataFrame(result)
+
+# graph result
+
+sns.heatmap(data = df)
+
+plt.show()
+
