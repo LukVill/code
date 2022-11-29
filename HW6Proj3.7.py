d1648d1e1b2fc3968345cb6c96aa92d5d008b8b4
LukVill <luke.lam.villanueva@gmail.com>
2022-11-29 05:49:14 -0800

    finally got the general matrix function to work whew

---
+++ HW6Proj3.7.py
@@ -0,0 +1,247 @@
+from icecream import ic
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# nxn
+# form nxn with 5 degrees on top, left, right, and 0 on bottom
+def nxn(row,col):
+    #set first row
+    matrix = np.array([5]*col)
+    matrix = np.mat(matrix)
+    ic(matrix)
+
+    #set edges to 5
+    for x in range(row-2):
+        r = np.array(5)
+        r = np.append(r,[None]*(col-2))
+        r = np.append(r,5)
+        matrix = np.r_[matrix,[r]]
+    # set bottom row to 0
+    matrix = np.r_[matrix, [[0]*col]]
+    return matrix
+
+
+# distanceParser
+# Function: increment count while negatively parsing left and up 
+# a matrix, COUNTING ONLY NONE's
+# Parameters: source matrix, target i (row), target j (col); 
+# OPTIONAL isNegative = bool (parsing left or right)
+# Returns: distance of target node from current node VIA NONE's
+def distanceParser(matrix, curI, curJ, tarI, tarJ, isNegative = False):
+    # Row and Col of matrix
+    nrow = matrix.shape[0]
+    ncol = matrix.shape[1]
+    
+    # Assert indices are within matrx
+    assert tarI >= 0 and tarI < nrow and tarJ >= 0 and tarJ < ncol
+
+    # Distance counter
+    count = 0
+    # Found target bool
+    isFound = False
+
+
+    # Normal forward parse
+    if isNegative == False:
+        # While target not found
+        while (isFound == False):
+            # Case 1: If col is at end, move to next row
+            if curJ == ncol-1:
+                curI += 1
+                curJ = 0
+            # Case 2: Normal parse thru cols
+            else:
+                curJ+=1
+
+            # Check if new index is None (temp node), increment count
+            if matrix[curI,curJ] == None:
+                count+=1
+            # Check if new index is target
+            if curI == tarI and curJ == tarJ:
+                return count
+            # Impossible to reach end and not find target
+            assert not(curI==nrow-1 and curJ==ncol-1 and \
+                isFound==False), "Parsed but not found"
+
+
+    # Negative parse
+    else:
+        # While target not found
+        while (isFound == False):
+            # Case 1: If col is at beginning, move to previous row
+            if curJ == 0:
+                curI -= 1
+                curJ = ncol-1
+            # Case 2: Normal parse thru cols
+            else:
+                curJ-=1
+
+            # Check if new index is None (temp node), increment count
+            if matrix[curI,curJ] == None:
+                count+=1
+            # Check if new index is target
+            if curI == tarI and curJ == tarJ:
+                return count
+            # Impossible to not have found target
+            assert not(curI==0 and curJ==0 and \
+                isFound==False), "Parsed but not found"
+
+# tempModel
+# function to create matrix of initial temperature and null
+# Parameters: initialCond matrix FORMED as object already
+# Assumptions: None's are the Temp nodes (aka, not heated)
+# Returns: matrix A, that is transition matrix of temperature nodes 
+# size (dim-2)^2; vertical array B of initial boundary vals
+def tempModel(initialCond):
+    # temp nodes exist within boundary lines (i.e. index = 1,1 
+    # and ends dim-2,dim-2 including)
+    # 
+    # find temp nodes by listing out indices of empty nodes
+    # formula to find temp nodes: find which order of Temp node adds
+    # into curr temp node, and then change A[i,order-1] = -1
+    # general equation, 
+    #                             IFF j>0     IFF j<DIM-1  IFF i>0   IFF i<DIM-1
+    # temp node = 1/4 ( e[i][j-1] + e[i][j+1] + e[i-1][j] e[i+1][j]
+
+    # Rows of initial condition matrix
+    nrows = initialCond.shape[0]
+    # Cols of initial condition matrix
+    ncols = initialCond.shape[1]
+    # Tracks current Temp node
+    node_count = 0
+
+    # Make A matrix for values of Temp nodes
+    # Find total of Nones in initialCond
+    totalNone = 0
+    for i in range(nrows):
+        for j in range(ncols):
+            if initialCond[i,j] == None:
+                totalNone += 1
+    ic(totalNone)
+    # Use totalNone to make transitionary matrix for Temp nodes
+    A = np.zeros((totalNone,totalNone))
+    # Make B matrix for temp initials for transitionary matrix
+    B = np.array([0]*totalNone)
+
+    # Make diagonal of A be all 4's
+    for x in range(len(A)):
+        A[x,x] = 4
+
+    # Check properties of cells
+    for i in range(nrows):
+        for j in range(ncols):
+            # if true, then current index is Temp node
+            if initialCond[i,j] == None:
+                node_count+=1
+
+                # Check up, left, right, and down of curr cell
+                # Case 1: Check up
+                # boundary check
+                if i > 0:
+                    # if curr index is Temp node
+                    if initialCond[i-1,j] == None:
+                        # Get distance
+                        d = distanceParser(initialCond, i, j, i-1, j, \
+                            isNegative = True)
+                        order = node_count-d
+                        # Mark A a -1 at order of Temp node
+                        A[node_count-1][order-1] = -1
+                    # if not Temp node
+                    else:
+                        # add value to B 
+                        B[node_count-1] += initialCond[i-1,j]
+                # Case 2: Check down
+                # boundary check
+                if i < nrows-1:
+                    # if curr index is Temp node
+                    if initialCond[i+1,j] == None:
+                        # Get distance
+                        d = distanceParser(initialCond, i, j, i+1, j, \
+                            isNegative = False)
+                        order = node_count + d
+                        # Mark A a -1 at order of Temp node
+                        A[node_count-1][order-1] = -1
+                    # if not Temp node
+                    else:
+                        # add value to B 
+                        B[node_count-1] += initialCond[i+1,j]
+                # Case 3: Check left
+                # boundary check
+                if j > 0:
+                    # if curr index is Temp node
+                    if initialCond[i,j-1] == None:
+                        # Get distance
+                        d = distanceParser(initialCond, i, j, i, j-1, \
+                            isNegative = True)
+                        order = node_count - d
+                        # Mark A a -1 at order of Temp node
+                        A[node_count-1][order - 1] = -1
+                    # if not Temp node
+                    else:
+                        # add value to B 
+                        B[node_count-1] += initialCond[i,j-1]
+                # Case 3: Check right
+                # boundary check
+                if j < ncols-1:
+                    # if curr index is Temp node
+                    if initialCond[i,j+1] == None:
+                        # Get distance
+                        d = distanceParser(initialCond, i, j, i, j+1, \
+                            isNegative = False)
+                        order = node_count + d
+                        # Mark A a -1 at order of Temp node
+                        A[node_count-1][order - 1] = -1
+                    # if not Temp node
+                    else:
+                        # add value to B 
+                        B[node_count-1] += initialCond[i,j+1]
+
+            # whether true or false, parse
+            # check if reached the end of initialCond matrix
+            if i == nrows-1 and j == ncols -1:
+                return A,B 
+            # if at last col, move to next row
+            if j == ncols - 1:
+                i += 1
+                j = 0
+            # else, normal column parse
+            else:
+                j+=1
+
+
+    # Assert to double check parsing logic
+    assert node_count == totalNone, "Node Index does not match number\
+    of Nones in original matrix"
+    
+
+# findTNodes
+# function to find values of T1-Tn
+# Parameters: transition matrix, initial conditions of temperature 
+# based on diagram
+# Returns: internal matrix of T's (EXCLUDES INITIAL CONDITIONS)
+def findTNodes(transMat, initialCond):
+    A = transMat
+    B = initialCond
+    ic(A.shape)
+    ic(B.shape)
+    Ainv = np.linalg.inv(A)
+    ic(Ainv)
+    # To solve for T, T = A^-1B
+    T = np.matmul(Ainv,B)
+    ic(T)
+
+# insertTNodes
+# Function: inserts TNodes into initial condition matrix (aka,
+# boundary matrix with initial temps) into the None values
+# Parameters: T matrix (for vals of T nodes); initial temp matrix
+# Returns: a new matrix with boundary temps AND temp nodes
+# (ready for graphing)
+def insertTNodes(Tmat,initialCond):
+    pass
+mat = nxn(5,5)
+A,B = tempModel(mat)
+ic(A)
+T = findTNodes(A,B)
+ic(T)
