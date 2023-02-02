7da4975528e954f420d64eec651042d92c288d01
LukVill <luke.lam.villanueva@gmail.com>
2023-02-02 13:44:57 -0800

    updated R stuff
    Signed-off-by: LukVill <luke.lam.villanueva@gmail.com>

--- R/test.R
+++ R/test.R
@@ -3,3 +3,19 @@ v <- c(TRUE,TRUE,TRUE)
 isTRUE(v)
 
 vapply(v, isTRUE, numeric(1))
+
+log(c(2,4,5))-exp(3)
+
+1:3 + 2:5
+
+length(5:12)
+length(-2:1)
+
+length(1:3)
+length(2:5)
+
+(1:10)[c(0,-5,-5,-20000)]
+
+x <- rep(TRUE,100)
+x[-1]
+x[2:length(x)]
