9d07c562557690ab4c8c4f6bddd20432fb9db32b
LukVill <luke.lam.villanueva@gmail.com>
2023-04-13 15:10:26 -0700

    Signed-off-by: LukVill <luke.lam.villanueva@gmail.com>

--- R/test.R
+++ R/test.R
@@ -1,34 +1,5 @@
-v <- c(TRUE,TRUE,TRUE)
-
-isTRUE(v)
-
-vapply(v, isTRUE, numeric(1))
-
-log(c(2,4,5))-exp(3)
-
-1:3 + 2:5
-
-length(5:12)
-length(-2:1)
-
-length(1:3)
-length(2:5)
-
-(1:10)[c(0,-5,-5,-20000)]
-
-x <- rep(TRUE,100)
-x[-1]
-x[2:length(x)]
-
-with(frame, {
-
-  num[num > 0]
-})
-
-mode(x[[1]][1])
-
-x <- factor(letters)
-
-tapply(x,1:26,max)
-
-library(package = "MASS")
+install.packages("random")
+library(random)
+help(random)
+set.seed(1234498253490)
+help(set.seed)
