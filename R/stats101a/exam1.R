f4c9cc54e68724619e8319b8b27ad2cb1b2b8f84
Luke Villanueva <luke.lam.villanueva@gmail.com>
2023-04-24 16:07:44 -0700

    Signed-off-by: Luke Villanueva <luke.lam.villanueva@gmail.com>

---
+++ R/stats101a/exam1.R
@@ -0,0 +1,31 @@
+batters <- read.csv(file = "C:/Users/Luke Villanueva/Downloads/battersExam.csv")
+covid <- read.csv(file = "C:/Users/Luke Villanueva/Downloads/covidExam.csv")
+
+la <- subset(covid, county == "Los Angeles")
+
+nrow(la)
+ncol(la)
+
+
+la$name
+
+colnames(la)
+
+# group by neighborhoods then get mean of confirmed cases
+la %>% group_by(name) %>% summarize(mean(confirmed_cases, na.rm = TRUE))
+print(la %>% group_by(name) %>% summarize(mean(confirmed_cases, na.rm = TRUE)), n = 353)
+
+glimpse(batters)
+
+
+nrow(batters)
+
+# SE based on sample standard deviation
+SE <- sd(batters$HR)/sqrt(length(batters$HR) - 1)
+
+#MOE
+MOE <- qt(0.95, df = length(batters$HR) - 1) * SE
+
+c(mean(batters$HR) - MOE, mean(batters$HR) + MOE)
+
+
